c09074891d5b2518fe9cd9449beb93fb9c6fa13a
“Jesse <jesse.krim@gmail.com>
2019-06-03 22:52:18 -0400

    Refactor to es6

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,26 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee {
+  constructor() { // Doing what prototype.constructor did in es5
+    super()  // must call before keyword 'this' 
+    // - brings in variables from parent constructor class in reference to this
+    // Same as what ForagerBee.call(this); is doing
+    // Since you're EXTENDING - must call SUPER
+
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+  }
+  
   // TODO..
-};
+
+
+forage () {
+  return "I am too old, let me play cards instead";
+}
+
+gamble (treasure) {
+  super.forage(this, treasure);
+  // this.treasureChest.push(treasure); 
+  // - bad if ForagerBee's forage function is 20 lines ;ong
+  }
+}
